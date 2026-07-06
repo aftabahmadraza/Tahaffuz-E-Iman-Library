efa0af5d3d10af76011152ce06fb289637ceccc4
aftabahmadraza <aftabpsb@gmail.com>
2026-07-07 03:47:56 +0530

    Update app.js

--- v2/assets/js/app.js
+++ v2/assets/js/app.js
@@ -3,6 +3,8 @@ Tahaffuz-E-Iman Library
 Main App
 ======================================= */
 
+alert("APP JS Loaded");
+
 document.addEventListener("DOMContentLoaded", () => {
 
     console.log("Tahaffuz-E-Iman Library Loaded");
