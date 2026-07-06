f18e3cecb2ee84d8d1a1065127e74f8b93ac9b58
aftabahmadraza <aftabpsb@gmail.com>
2026-07-07 00:06:00 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,46 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Tahaffuz-E-Iman Library Loaded Successfully");
-});
+document.addEventListener("DOMContentLoaded", loadLatestQuestions);
+
+async function loadLatestQuestions() {
+
+    const container = document.getElementById("latestQuestions");
+
+    if (!container) return;
+
+    try {
+
+        const response = await fetch("database/index.json");
+        const questions = await response.json();
+
+        container.innerHTML = "";
+
+        questions.forEach(item => {
+
+            container.innerHTML += `
+
+            <div class="question-card">
+
+                <h3>${item.title}</h3>
+
+                <p>Category : ${item.category}</p>
+
+                <a href="question.html?id=${item.id}">
+                    View Answer →
+                </a>
+
+            </div>
+
+            `;
+
+        });
+
+    }
+
+    catch(error){
+
+        container.innerHTML="<p>Questions could not be loaded.</p>";
+
+        console.log(error);
+
+    }
+
+}
